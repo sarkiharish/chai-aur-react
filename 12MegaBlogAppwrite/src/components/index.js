7ea2f385e2a2df6dbbd3e1d1536cb162bc520831
sarkiharish <sarkiharish42@gmail.com>
2023-10-29 10:08:35 +0545

    addition of  react-hook-form in app

--- 12MegaBlogAppwrite/src/components/index.js
+++ 12MegaBlogAppwrite/src/components/index.js
@@ -1,4 +1,21 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Logo from "./Logo";
+import Container from "./container/Container";
+import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./Input";
+import Button from './Button'
+import Select from "./Select";
+import PostCard from "./PostCard";
 
-export {Header, Footer}
+export {
+     Header,
+     Footer,
+     Logo,
+     Container,
+     LogoutBtn,
+     Input,
+     Button,
+     Select,
+     PostCard
+}
